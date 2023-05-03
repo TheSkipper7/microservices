4d26a131e0f9488cee4f05a92777f0b172775d3d
TheSkipper7 <118884769+TheSkipper7@users.noreply.github.com>
2023-05-03 15:34:30 +0530

    Add files via upload

---
+++ pensionerDetail/src/main/java/com/ms/pensionerDetail/PensionerDetailApplication.java
@@ -0,0 +1,13 @@
+package com.ms.pensionerDetail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensionerDetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PensionerDetailApplication.class, args);
+	}
+
+}
---
+++ pensionerDetail/src/test/java/com/ms/pensionerDetail/PensionerDetailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.pensionerDetail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PensionerDetailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
